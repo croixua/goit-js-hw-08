79f94b0f2d61615cae4b92a4eb6aeebb669fba8a
croixua <croixua@gmail.com>
2022-01-25 10:08:00 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryRef = document.querySelector('.gallery');
 const itemsGallaryMarkup = createGallaryItemsMarkup(galleryItems);
