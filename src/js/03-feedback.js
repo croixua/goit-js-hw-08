d17c30bbcc4b408970c187c14994b8ffd301fe56
croixua <croixua@gmail.com>
2022-01-29 00:13:46 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import Throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const emailRef = formRef.elements.email;
+const messageRef = formRef.elements.message;
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+updateOutput();
+
+const feedbackFormState = {
+  email: '',
+  message: '',
+};
+
+emailRef.addEventListener('input', Throttle(setFormEmail, 500));
+messageRef.addEventListener('input', Throttle(setFormMessage, 500));
+formRef.addEventListener('submit', onSubmit);
+
+function setFormMessage() {
+  feedbackFormState.message = messageRef.value;
+
+  if (emailRef.value !== '') {
+    feedbackFormState.email = emailRef.value;
+  }
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
+  } catch (error) {}
+}
+
+function setFormEmail() {
+  feedbackFormState.email = emailRef.value;
+  if (messageRef.value !== '') {
+    feedbackFormState.message = messageRef.value;
+  }
+
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
+  } catch (error) {}
+}
+
+function updateOutput() {
+  try {
+    const savedFormState = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsedFormState = JSON.parse(savedFormState);
+    emailRef.value = `${parsedFormState.email}`;
+    messageRef.value = `${parsedFormState.message}`;
+  } catch (error) {}
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  feedbackFormState.email = '';
+  feedbackFormState.message = '';
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
+  formRef.reset();
+}
