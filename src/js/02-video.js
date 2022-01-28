014b8a73f9d4c49832cf95a961dc2a67805dc8d0
croixua <croixua@gmail.com>
2022-01-28 22:45:38 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+import Throttle from 'lodash.throttle';
 
+const handlerCurrentTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+};
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', Throttle(handlerCurrentTime, 1000));
+
+player
+  .setCurrentTime(currentTime - 2)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
